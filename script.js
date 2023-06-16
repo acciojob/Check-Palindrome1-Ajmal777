40028ef3e2ee98211eacecbca36f392be87e27de
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-16 17:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.replace(" ", "");
+	let l = 0, r = str.length-1;
+	while(l < r){
+		if(str[l].toLowerCase() !== str[r].toLowerCase()) return false;
+		l++;
+		r--;
+	}
+	return true;
 }
 module.exports = palindrome
